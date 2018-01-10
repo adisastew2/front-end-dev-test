889ae21f36e8a303041df0a59776b7eae01d05b1
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-09 23:21:53 -0800

    create header component

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+  return (
+    <header>
+      <img id='logo' src='../../assets/igniter-logo-white.png' />
+    </header>
+  );
+}
+
+export default Header;
