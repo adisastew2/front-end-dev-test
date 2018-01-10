8f724be0345d65054a67d52c03cd775a4c3eda68
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-10 13:35:00 -0800

    Start pages component

---
+++ src/components/pages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Pages(){
+  return (
+    <div>
+      <img src='../../assets/page-icon.svg'/>
+      <p>Lorem ipsum</p>
+    </div>
+  );
+}
+
+export default Pages;
