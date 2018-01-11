0d92e3950edc4fe73714fbc3b069323a31740770
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-11 11:40:36 -0800

    Start social footer component

---
+++ src/components/social.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Social(){
+  return (
+    <div>
+      <div>
+        <input type='text' placeholder='something goes here' />
+        <button>click</button>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Social;
