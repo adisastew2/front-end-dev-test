4cd45287dffd62d7ac3285b4f5baeb48263f097e
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-11 15:45:19 -0800

    Add classes for head style

--- src/components/checkerbox.jsx
+++ src/components/checkerbox.jsx
@@ -5,11 +5,11 @@ import {Row, Col} from 'react-bootstrap';
 function Checkerbox(){
   return (
     <div>
-      <h1>Lorem Ipsum</h1>
+      <h1 className='bigHead'><span className='bigHeadL'>Lorem</span> <span className='bigHeadR'>Ipsum</span></h1>
       <Row>      
         <Col md={6}>
-          <h2>Lorem Ipsum</h2>
-          <h3>Lorem Ipsum</h3>
+          <h2 className='midHead'>Lorem Ipsum</h2>
+          <h3 className='smlHead'>Lorem Ipsum</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Col>
         <Col md={6}><img className='checkImg animated slideInRight' src='../../assets/hands.jpg'/></Col>
@@ -18,8 +18,8 @@ function Checkerbox(){
       <Row>
         <Col md={6}><img className='checkImg animated slideInLeft' src='../../assets/data.jpg'/></Col>
         <Col md={6}>
-          <h2>Lorem Ipsum</h2>
-          <h3>Lorem Ipsum</h3>
+          <h2 className='midHead'>Lorem Ipsum</h2>
+          <h3 className='smlHead'>Lorem Ipsum</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Col> 
       </Row>
