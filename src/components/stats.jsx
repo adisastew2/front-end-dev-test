8d03ad86815b95b6f23588b037eb715ad16ebab4
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-10 17:44:19 -0800

    Start stats component

---
+++ src/components/stats.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Stats(){
+  return (
+    <div>
+      <h1>Lorem Ipsum</h1>
+      <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button>do thing</button>
+      </div>
+      <div>
+        <span>
+          <h1>240%</h1>
+          <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
+        </span>
+        <span>
+          <h1>105%</h1>
+          <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
+        </span>
+        <span>
+          <h1>159%</h1>
+          <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Stats;
