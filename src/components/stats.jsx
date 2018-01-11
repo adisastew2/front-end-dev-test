6021722924ba1b0bdf315986d00d5ff053642ae9
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-11 09:44:13 -0800

    Add rows and columns

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
+import {Row, Col} from 'react-bootstrap';
 
 function Stats(){
   return (
     <div>
       <h1>Lorem Ipsum</h1>
-      <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>do thing</button>
-      </div>
-      <div>
-        <span>
+      <Row>
+        <Col md={3}></Col>
+        <Col md={3}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Col>
+        <Col md={3}><button>do thing</button></Col>
+        <Col md={3}></Col>
+      </Row>
+      <Row>
+        <Col md={3}>
           <h1>240%</h1>
           <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
-        </span>
-        <span>
+        </Col>
+        <Col md={3}>
           <h1>105%</h1>
           <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
-        </span>
-        <span>
+        </Col>
+        <Col md={3}>
           <h1>159%</h1>
           <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
-        </span>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
 }
