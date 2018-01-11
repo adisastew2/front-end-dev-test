1e759c1ac46a8b3690b78b456d037e493571de06
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-11 14:54:05 -0800

    Change size of columns add id to row for styling

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -10,19 +10,19 @@ function Stats(){
         <Col md={3}>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </Col>
-        <Col md={3}><button>do thing</button></Col>
+        <Col md={3}><button className='button'>button</button></Col>
         <Col md={3}></Col>
       </Row>
-      <Row>
-        <Col md={3}>
+      <Row id='stats'>
+        <Col md={4}>
           <h1>240%</h1>
           <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
         </Col>
-        <Col md={3}>
+        <Col md={4}>
           <h1>105%</h1>
           <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
         </Col>
-        <Col md={3}>
+        <Col md={4}>
           <h1>159%</h1>
           <p>Lorem ipsum dolor sit amet Lorem ipsum dolor ipsum dolor</p>
         </Col>
