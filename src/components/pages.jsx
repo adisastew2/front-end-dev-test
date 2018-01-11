5387ab92f43b394c045ab0e249758377c0b20d18
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-11 09:47:30 -0800

    Add rows and columns

--- src/components/pages.jsx
+++ src/components/pages.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
+import {Row, Col} from 'react-bootstrap';
+
 function Pages(){
   return (
-    <div>
-      <img src='../../assets/page-icon.svg'/>
-      <p>Lorem ipsum</p>
+    <div id='pages'>
+      <Row>
+        <Col md={3}></Col>
+        <Col md={3} >
+          <img className='left' src='../../assets/page-icon.svg'/>
+          <p>Lorem ipsum</p>
+        </Col>
+        <Col md={3} >
+          <img className='left' src='../../assets/page-icon.svg'/>
+          <p>Lorem ipsum</p>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
+
+      <Row>
+        <Col md={3}></Col>
+        <Col md={3} >
+          <img className='left' src='../../assets/page-icon.svg'/>
+          <p>Lorem ipsum</p>
+        </Col>
+        <Col md={3} >
+          <img className='left' src='../../assets/page-icon.svg'/>
+          <p>Lorem ipsum</p>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
     </div>
+
   );
 }
 
