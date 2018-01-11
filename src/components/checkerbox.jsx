b5eecd3a52d331399c95d8c0b2fc391fd9135f24
Adisa Stewart <adisa.stewart1988@gmail.com>
2018-01-10 17:02:35 -0800

    Start checkerboard component

---
+++ src/components/checkerbox.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Checkerbox(){
+  return (
+    <div>
+
+      <div>
+        <span>
+          <h2>Lorem Ipsum</h2>
+          <h3>Lorem Ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </span>
+        <span><img src='../../assets/hands.jpg'/></span>
+      </div>
+
+      <div>
+        <span><img src='../../assets/data.jpg'/></span>
+        <span>
+          <h2>Lorem Ipsum</h2>
+          <h3>Lorem Ipsum</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </span>   
+      </div>
+
+    </div>
+  );
+}
+
+export default Checkerbox;
